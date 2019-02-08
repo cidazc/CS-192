750895fca8c9e5a798e2e2ca4bfb6fa2626fbf59
Allan Mariano <37423739+AllanMatthewMariano@users.noreply.github.com>
2019-02-08 12:37:13 +0800

    stuff

--- manage.py
+++ manage.py
@@ -1,3 +1,29 @@
+
+
+
+
+
+"""
+
+Allan Matthew B. Mariano 
+2015-05804
+
+
+“This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof.
+ Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, 
+ Diliman for the AY 2018-2019”.
+
+"""
+
+
+
+
+
+
+
+
+
+
 #!/usr/bin/env python
 import os
 import sys
--- my_project/views.py
+++ my_project/views.py
@@ -1,3 +1,20 @@
+
+
+"""
+
+Allan Matthew B. Mariano 
+2015-05804
+
+
+“This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof.
+ Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, 
+ Diliman for the AY 2018-2019”.
+
+"""
+
+
+
+
 from django.contrib.auth.models import User
 from django.conf.urls import url
 from django.http import HttpResponse
