1a9dfe0eb4ef3f086cc0d57d50b53e806382be09
Cid Azcarraga <joseazcarragaiv@gmail.com>
2019-02-27 13:19:45 +0800

    reduced size of testing project

--- my_project/views.py
+++ my_project/views.py
@@ -3,12 +3,12 @@
 
 """
 
-Allan Matthew B. Mariano 
+Allan Matthew B. Mariano
 2015-05804
 
 
 “This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof.
- Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines, 
+ Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering, University of the Philippines,
  Diliman for the AY 2018-2019”.
 
 """
@@ -45,7 +45,7 @@ This is a course requirement for CS 192 Software Engineering II under the superv
 Code History:
        -CID AZCARRAGA|FEB-7-2019| Creation of this Code
        -CID AZCARRAGA|FEB-*-2019| Addition of License, Code History, and other Information to Code
-    
+
 Information:
      file creation: This was generated February 7, 2019.
      development group: Salin - Group 2
@@ -61,7 +61,51 @@ from django.http import HttpResponse
 from my_project.models import Translation
 
 def home(request):
-    return HttpResponse('<h1> Welcome </h1>')
+    #return HttpResponse('<h1> Welcome </h1>')
+    try:
+        html = '''<!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width">
+            <title>My App</title>
+            <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <style>
+              body{
+                background-color: yellowgreen
+              }
+            </style>
+          </head>
+          <body>
+            <div>
+              <div class="jumbotron text-center">
+                <h1>Salin</h1>
+              </div>
+              <div id="app">
+                <h3>Log-in</h3>
+                <input placeholder="Username">
+                <input placeholder="Password">
+                <br>
+                <a href = "login.html">Log-in</a>
+              </div>
+              <script src="index.js"></script>
+            </div>
+          </body>
+        </html>'''
+        return HttpResponse(html)
+    except:
+        title = 'Error'
+        html = '''<!DOCTYPE html>
+        <html>
+        <head>
+            <title>''' + title + '''</title>
+        </head>
+        <body>
+            <h1>Error!!</h1>
+        </body>
+        </html>'''
+        return HttpResponse(html)
 
 def delete(request):
     try:
@@ -199,7 +243,7 @@ def addTranslation(request):
     </body>
     </html>'''
     return HttpResponse(html)
-    #except: 
+    #except:
     '''
         title = 'Error'
         html = <!DOCTYPE html>
@@ -211,7 +255,7 @@ def addTranslation(request):
             <h1>Error!!</h1>
         </body>
         </html>
-        return HttpResponse(html) 
+        return HttpResponse(html)
 
     '''
 
