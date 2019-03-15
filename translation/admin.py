e8627d477b85fbd4494af6b5941f0d2de89aa042
Reyster Fresco <reysterf@gmail.com>
2019-03-15 23:11:39 +0800

    Try to start from scratch

--- translation/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Translation
-from .models import Search #
-
-admin.site.register(Translation)
-admin.site.register(Search)
