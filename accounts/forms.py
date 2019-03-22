2f246f764b8e81907a8a9c4811ae6b6ae86d7461
Cid Azcarraga <joseazcarragaiv@gmail.com>
2019-03-22 11:43:36 +0800

    can delete user but not acknowledged yet

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 
+from .models import Username
 
 class UserForm(ModelForm):
     class Meta:
         model = User
+        fields = '__all__'
+
+class UsernameForm(ModelForm):
+    class Meta:
+        model = Username
         fields = '__all__'
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import UserForm
-
+from .forms import UserForm, UsernameForm
+from django.contrib.auth.models import User
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
@@ -28,9 +28,15 @@ class SignUp(generic.CreateView):
 
 def remove_user(request):
     print("It is here")
-    form = UserForm(request.POST, instance = request.user)
+    form = UsernameForm(request.POST, instance = request.user)
     if form.is_valid:
         print(request.POST.copy())
+        data = request.POST.copy()
+        delete = data.get('username')
+        print(delete)
+        u = User.objects.get(username = delete)
+        u.delete()
+
     else:
         messages.error(request,form.errors)
     context = {'form': form}
